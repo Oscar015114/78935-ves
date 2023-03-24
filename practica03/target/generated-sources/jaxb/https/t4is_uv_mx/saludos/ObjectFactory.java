df217c3f8bbda109f37072960c89f0c9d4978cef
Oscar015114 <111479477+Oscar015114@users.noreply.github.com>
2023-03-23 23:58:06 -0600

    Add files via upload

---
+++ practica03/target/generated-sources/jaxb/https/t4is_uv_mx/saludos/ObjectFactory.java
@@ -0,0 +1,103 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.03.23 a las 11:50:04 PM CST 
+//
+
+
+package https.t4is_uv_mx.saludos;
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the https.t4is_uv_mx.saludos package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: https.t4is_uv_mx.saludos
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SaludarRequest }
+     * 
+     */
+    public SaludarRequest createSaludarRequest() {
+        return new SaludarRequest();
+    }
+
+    /**
+     * Create an instance of {@link SaludarResponse }
+     * 
+     */
+    public SaludarResponse createSaludarResponse() {
+        return new SaludarResponse();
+    }
+
+    /**
+     * Create an instance of {@link BuscarRequest }
+     * 
+     */
+    public BuscarRequest createBuscarRequest() {
+        return new BuscarRequest();
+    }
+
+    /**
+     * Create an instance of {@link BuscarResponse }
+     * 
+     */
+    public BuscarResponse createBuscarResponse() {
+        return new BuscarResponse();
+    }
+
+    /**
+     * Create an instance of {@link ModificarRequest }
+     * 
+     */
+    public ModificarRequest createModificarRequest() {
+        return new ModificarRequest();
+    }
+
+    /**
+     * Create an instance of {@link ModificarResponse }
+     * 
+     */
+    public ModificarResponse createModificarResponse() {
+        return new ModificarResponse();
+    }
+
+    /**
+     * Create an instance of {@link EliminarRequest }
+     * 
+     */
+    public EliminarRequest createEliminarRequest() {
+        return new EliminarRequest();
+    }
+
+    /**
+     * Create an instance of {@link EliminarResponse }
+     * 
+     */
+    public EliminarResponse createEliminarResponse() {
+        return new EliminarResponse();
+    }
+
+}
